e49a8a4d2b6b6d3a74e84801af2a36e1b5b6d5cb
Alexander Polishchuk <apolishchuk52@gmail.com>
2020-03-04 18:29:22 +0300

    Registred integration pytest marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,37 @@ from rest_framework import test as drf_test
 
 from cc_framework.blockchain import connectors, models
 
+# TODO: Node ping before integration tests
+
+# console options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--integration',
+        action='store_true',
+        default='',
+        help='Run integration tests with main test suite.',
+    )
+
+
+# pytest hooks
+
+
+def pytest_runtest_setup(item):
+    """Pytest hook that called before each test.
+
+    Docs:
+        https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_runtest_setup
+
+    Args:
+        item: Pytest item object (conceptually is test).
+    """
+    is_integration_session = item.config.getoption('--integration')
+    is_integration_test = bool(list(item.iter_markers(name='integration')))
+    if not is_integration_session and is_integration_test:
+        pytest.skip('skipped integration test')
+
 
 @pytest.fixture
 def client():
@@ -12,9 +43,9 @@ def client():
 @pytest.fixture
 def bitcoin_core_connector():
     return connectors.btc.BitcoinCoreConnector(
-        rpc_username='bitcoin',
-        rpc_password='qwerty54',
-        rpc_host='http://example.com',
+        rpc_username='testnet_user',
+        rpc_password='testnet_pass',
+        rpc_host='127.0.0.1',
         rpc_port=18332,
     )
 
