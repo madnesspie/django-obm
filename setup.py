24d74f2afad054e10547ad12d1196f19cdc17adc
Alexander Polishchuk <apolishchuk52@gmail.com>
2020-03-15 16:43:11 +0300

    Fixed long description format

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     license='GNU Lesser General Public License v3 or later (LGPLv3+)',
     description='A Django app for receiving payments in cryptocurrencies.',
     long_description=README,
-    long_description_content_type='text/x-rst',
     url='https://github.com/HelloCreepy/django-cryptocurrency-framework',
     author='Alexander Polishchuk',
     author_email='apolishchuk52@gmail.com',
