1667ab1d4a4c501d795b5869a5cf9e5427ec83cd
Alexander Polishchuk <apolishchuk52@gmail.com>
2020-03-17 12:27:38 +0300

    Dropped celery from example project

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,7 +1,7 @@
 """example URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import urls
 from django.contrib import admin
-
-from cc_framework.rest import urls as rest_urls
+from django.urls import path
 
 urlpatterns = [
-    urls.path(r'', urls.include(rest_urls.urlpatterns)),
-    urls.path(r'admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
--- example/testapp/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-
-from cc_framework.blockchain import models
-
-
-@shared_task
-def process_receipts():
-    models.Node.objects.process_receipts()
-    return 'OK'
