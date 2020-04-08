a794d69007b4e762758cb2c2362d84c51298516b
Alexander Polishchuk <apolishchuk52@gmail.com>
2020-04-08 10:13:41 +0300

    Deleted built-in connectors

--- django_obm/connectors/__init__.py
+++
@@ -1,7 +0,0 @@
-from django_obm.connectors import _base as base
-from django_obm.connectors import _exceptions as exceptions
-from django_obm.connectors import _registry
-from django_obm.connectors import _utils as utils
-from django_obm.connectors import btc
-
-registry = _registry.ConnectorRegistry()  # pylint: disable=invalid-name
