24fdd6316ff32988904c08524afc644d508ba8c7
Alexander Polishchuk <apolishchuk52@gmail.com>
2020-04-24 17:03:07 +0300

    Added logger

---
+++ django_obm/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+from django.conf import settings
+
+logging.addLevelName(logging.DEBUG, "🐛 DEBUG")
+logging.addLevelName(logging.INFO, "📑 INFO")
+logging.addLevelName(logging.WARNING, "⚠️ WARNING")
+logging.addLevelName(logging.ERROR, "🚨 ERROR")
+logging.addLevelName(logging.CRITICAL, "💥 CRITICAL")
+
+
+def info_filter(record):
+    return record.levelno < logging.WARNING
+
+
+def get(name):
+    logger = logging.getLogger(name)
+    return logger
